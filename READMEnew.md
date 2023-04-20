2a0801944a61501356e95586ce5f6b55d61e34fa
Chris Daniels <danielschris96@gmail.com>
2023-04-19 21:52:58 -0700

    updated markdown generator with license info and better prompts

---
+++ READMEnew.md
@@ -0,0 +1,35 @@
+# asdf
+  ![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+
+  ## Description
+  asdf
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
+  ## Installation
+  To install dependencies, use the command `npm i`.
+  
+  ## Usage
+  asdf
+
+  
+## License
+
+This project is licensed under the [MIT](https://opensource.org/licenses/MIT) license.
+
+
+  ## Contributing
+  asdf
+
+  ## Tests
+  To run tests, use the command `npm test`.
+
+  ## Questions
+  Email: asd   
+  GitHub: https://github.com/asdf
--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,18 +1,60 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch(license) {
+    case 'MIT':
+      return '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)';
+    case 'APACHE 2.0':
+      return '![License: Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)';
+    case 'GPL 3.0':
+      return '![License: GPL 3.0](https://img.shields.io/badge/License-GPLv3-blue.svg)';
+    case 'BSD 3':
+      return '![License: BSD 3-Clause](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)';
+    case 'None':
+      return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch(license) {
+    case 'MIT':
+      return 'https://opensource.org/licenses/MIT';
+    case 'APACHE 2.0':
+      return 'https://opensource.org/licenses/Apache-2.0';
+    case 'GPL 3.0':
+      return 'https://www.gnu.org/licenses/gpl-3.0';
+    case 'BSD 3':
+      return 'https://opensource.org/licenses/BSD-3-Clause';
+    case 'None':
+      return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'None') {
+    return `
+## License
+
+This project is licensed under the [${license}](${renderLicenseLink(license)}) license.
+`
+  } else {
+    return `
+## License
+    
+None
+`;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
+
   ## Description
   ${data.description}
 
@@ -25,19 +67,18 @@ function generateMarkdown(data) {
   * [Questions](#questions)
 
   ## Installation
-  Run '${data.installation}' in the terminal.
+  To install dependencies, use the command \`${data.installation}\`.
   
   ## Usage
   ${data.usage}
 
-  ## License
-  ${data.license}
+  ${renderLicenseSection(data.license)}
 
   ## Contributing
   ${data.contribute}
 
   ## Tests
-  Run '${data.tests}' in the terminal.
+  To run tests, use the command \`${data.tests}\`.
 
   ## Questions
   Email: ${data.email}   
